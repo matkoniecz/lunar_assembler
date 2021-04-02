0a1db8fd20bb8111656e504e63c80bbf6d75c2ab
Dariusz Niemczyk <palid@phoelid.me>
2021-04-02 20:39:10 +0200

    Make things actually work, kinda.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
 import L from 'leaflet';
 
-export const setup_map = () => {
-  var map = L.map('map').setView([50, 20], 18);
-  var mapLink: string = 
-      '<a href="https://openstreetmap.org">OpenStreetMap</a>';
-  L.tileLayer(
-      'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; ' + mapLink + ' Contributors',
-      maxZoom: 18,
-      }).addTo(map);
-};
+export function init() {
+  const map = new L.Map('map');
+  map.setView([50, 20], 18);
+  var mapLink: string = '<a href="https://openstreetmap.org">OpenStreetMap</a>';
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; ' + mapLink + ' Contributors',
+    maxZoom: 18,
+  }).addTo(map);
+}
