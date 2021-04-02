5191f14c60dab37a7786593519957f0ec79c5e80
Mateusz Konieczny <matkoniecz@gmail.com>
2021-04-02 15:35:44 +0200

    add minimal code using leaflet

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
-  }
-  return a + b;
+import L from 'leaflet';
+
+export const setup_map = () => {
+  var map = L.map('map').setView([50, 20], 18);
+  var mapLink: string = 
+      '<a href="https://openstreetmap.org">OpenStreetMap</a>';
+  L.tileLayer(
+      'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; ' + mapLink + ' Contributors',
+      maxZoom: 18,
+      }).addTo(map);
 };
