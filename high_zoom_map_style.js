1e458d65b28eb1b08d787bba2a5ff7f4a047a6cf
Mateusz Konieczny <matkoniecz@gmail.com>
2021-04-02 12:26:34 +0200

    two more cases of greenery added to map style

--- high_zoom_map_style.js
+++ high_zoom_map_style.js
@@ -124,7 +124,7 @@ fillColoring(feature){
     if(["park", "pitch"].includes(feature.properties["leisure"]) || feature.properties["landuse"] === "village_green") {
       return "#c8facc";
     }
-    if(["grass", "allotments", "orchard"].includes(feature.properties["landuse"]) || ["grassland", "meadow"].includes(feature.properties["natural"]) || ["garden"].includes(feature.properties["leisure"])) {
+    if(["grass", "allotments", "orchard"].includes(feature.properties["landuse"]) || ["grassland", "meadow", "scrub", "heath"].includes(feature.properties["natural"]) || ["garden"].includes(feature.properties["leisure"])) {
       return "#a2ce8d";
     }
     if(feature.properties["man_made"] === "bridge") {
