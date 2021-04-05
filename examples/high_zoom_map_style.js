8a6fd3eef5ddc10fea67e40d8f6a8ae766ff4c05
Mateusz Konieczny <matkoniecz@gmail.com>
2021-04-05 08:26:11 +0200

    add construction to heavy landuse

--- examples/high_zoom_map_style.js
+++ examples/high_zoom_map_style.js
@@ -112,7 +112,7 @@ fillColoring(feature){
     if(feature.properties["natural"] === "wood" || feature.properties["landuse"] === "forest")  {
       return "green";
     }
-    if(["industrial", "railway", "quarry"].includes(feature.properties["landuse"])){
+    if(["industrial", "railway", "quarry", "construction"].includes(feature.properties["landuse"])){
         return "#efdfef";
     }
     if(["residential", "highway", "retail", "commercial", "garages"].includes(feature.properties["landuse"]) || ["school", "kidergarten", "university"].includes(feature.properties["amenity"])){
