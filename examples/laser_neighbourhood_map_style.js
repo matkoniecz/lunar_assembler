222a51be080d8b7a462b4b412d11a61a060343de
Mateusz Konieczny <matkoniecz@gmail.com>
2021-05-28 17:57:58 +0200

    fix rendering in presence of parking spaces

--- examples/laser_neighbourhood_map_style.js
+++ examples/laser_neighbourhood_map_style.js
@@ -91,8 +91,7 @@ function highZoomLaserMapStyle() {
       if (feature.properties["building"] != null) {
         return "#B45A00";
       }
-      if (mapStyle.motorizedRoadValuesArray().includes(feature.properties["area:highway"]) || feature.properties["area:highway"] === "bicycle_crossing"
-      || feature.properties["lunar_assembler_merge_group"] == "highway_carriageway_layer") {
+      if (feature.properties["lunar_assembler_merge_group"] == "area:highway_carriageway_layer") {
         if (feature.properties["lunar_assembler_cloned_for_pattern_fill"] == "yes") {
           return "#808080";
         }
