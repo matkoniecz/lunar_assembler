41415db4f8b0cd451afa6ce8ffcaac3530f28d27
Mateusz Konieczny <matkoniecz@gmail.com>
2021-06-04 14:32:10 +0200

    explain better

--- examples/laser_neighbourhood_map_style.js
+++ examples/laser_neighbourhood_map_style.js
@@ -279,7 +279,7 @@ function highZoomLaserMapStyle() {
       var found = undefined;
       while (i--) {
         var feature = dataGeojson.features[i];
-        // playground and garden within school
+        // playground and garden within school - private but accessible to children
         if (["way/940940433", "way/947218894"].includes(feature.id)) {
           dataGeojson.features.splice(i, 1); // remove matching element
         }
